ba58e6f872860f71a84d076c47eaa5220cf98f4b
Kroyx <66555957+kroyx@users.noreply.github.com>
2023-10-20 10:08:58 +0200

    Agrega código opcional para bloquear el schema a los usuarios no autenticados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 import { AppConfig } from './config/app-config';
 import { JoiValidationSchema } from './config/joi-validation';
 import { ItemsModule } from './items/items.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -24,6 +25,25 @@ import { AuthModule } from './auth/auth.module';
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       includeStacktraceInErrorResponses: false,
     }),
+    // ! Bloquea el Schema si no se envía un token válido
+    // GraphQLModule.forRootAsync<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   imports: [AuthModule],
+    //   inject: [JwtService],
+    //   useFactory: async (jwtService: JwtService) => ({
+    //     playground: false,
+    //     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    //     plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    //     includeStacktraceInErrorResponses: false,
+    //     context({ req }) {
+    //       const token = req.headers.authorization?.replace('Bearer ', '');
+    //       if (!token) throw Error('Token is needed');
+    //
+    //       const payload = jwtService.decode(token);
+    //       if (!payload) throw Error('Token is not valid');
+    //     },
+    //   }),
+    // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
